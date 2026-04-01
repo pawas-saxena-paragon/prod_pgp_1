cb9ceaefe2d160ef16e7fd64dfca334a5d5dfd38
pawas-saxena-paragon <70215948+pawas-saxena-paragon@users.noreply.github.com>
2026-04-01 14:10:17 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/gmail/config.ts
@@ -0,0 +1,20 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a gmail
+ */
+const config: IIntegrationConfig = {
+  active: true,
+  description: 'Send email with Gmail',
+  overviewText: `Connect to your Gmail account to manage your emails and drafts in Gmail. Increase your team’s productivity by keeping your Gmail account up to date - without manual data entry. 
+                
+Our Gmail integration enables you to:  
+          
+• Send email and drafts from your Gmail account
+• Read and extract data from incoming emails in your Gmail account inbox
+• Receive notifications upon receiving mail in your Gmail account inbox`,
+  showWatermark: false,
+  workflowDisplayOrder: [],
+};
+
+export default config;
