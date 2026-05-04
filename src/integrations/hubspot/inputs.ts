5e7ca042f2107dd9642e16e33eb62066405a8a76
pawas-saxena-paragon <70215948+pawas-saxena-paragon@users.noreply.github.com>
2026-05-04 06:52:28 +0000

    ⤵️ Sync changes from remote

--- src/integrations/hubspot/inputs.ts
+++ src/integrations/hubspot/inputs.ts
@@ -3,6 +3,25 @@ import { createInputs } from '@useparagon/integrations/hubspot';
 /**
  * define inputs here which can be used across workflows
  */
-const integrationInputs = createInputs({});
+const integrationInputs = createInputs({
+  field_title: {
+    id: '9cfd6e31-3b60-420b-b8a2-ab04bfce92db',
+    title: 'fieldTitle',
+    tooltip: '',
+    required: false,
+    type: 'custom_dropdown',
+    key: 'fieldtitle',
+    customDropdownOptions: [
+      {
+        label: 'Example Field 1',
+        value: 'field-1',
+      },
+      {
+        label: 'Example Field 2',
+        value: 'field-2',
+      },
+    ],
+  },
+});
 
 export default integrationInputs;
